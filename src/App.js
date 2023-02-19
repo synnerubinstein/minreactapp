779f29f0ad848f8f0d404fe5886db6224bb38a39
Emma Vikene Løvstrand <emlo005@student.kristiania.no>
2023-02-19 18:12:51 +0100

    spinning matias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './matias.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lol Hehehe :) <code>src/App.js</code>  reload.
+          Lol <code>src/App.js</code>  reload.
         </p>
         <a
           className="App-link"
