a3b104656587321c46a48a65e4ac9ab60b7716f7
MatiasMogensen <89384310+MatiasMogensen@users.noreply.github.com>
2023-03-05 16:36:30 +0100

    Push request
    Har lagt til en funksjon som lager en push request med brukernavn og svar. Funksjonen kjører hver gang man oppdaterer siden, gjorde dette for å sjekke at man ikke ødelegger noe når man gjør fremtidige forandringer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './matias.png';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Lol Hehehe Hei jeg vil bare være aktiv på Github hehehe  <code>src/App.js</code>  reload.
+          Lol Hehehe Hei jeg vil bare være aktiv på Github litt mer :) <code>src/App.js</code>  reload.
         </p>
         <a
           className="App-link"
@@ -19,8 +22,29 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
+
+function PostRequest(brukernavn, svar){
+  axios({
+    method: 'post',
+    url:'http://192.168.198.49:3000',
+    data: {
+      brukernavn,
+      svar
+    }
+  })
+  .then((response)=>{
+    console.log(response);
+  },(error)=>{
+    console.log(error)
+  })
+}
+
+var userInput = "Matias"
+var userAnswer = "Superstar coder"
+PostRequest(userInput, userAnswer)
 
 export default App;
