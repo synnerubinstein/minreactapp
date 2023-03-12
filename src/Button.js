68dcc82ae0fd93d418613d51abb2d8446530d87b
maypaytang <may_elisabeth98@hotmail.com>
2023-03-12 15:50:04 +0100

    knappefiks
    +gjorde knappene finere
    +melding om at b er rett

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,20 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 
 const Buttons = () => {
+    const [message, setMessage] = useState('');
+    const handleButtonClick = (event) => {
+    if (event.target.id === 'buttonB') {
+    setMessage('correct');
+}
+};
     return(
         <div id='buttonDiv'>
-            <button type="button" className='buttons' id='buttonA'>A</button>
-            <button type="button" className='buttons' id='buttonB'>B</button>
-            <button type="button" className='buttons' id='buttonC'>C</button>
-            <button type="button" className='buttons' id='buttonD'>D</button>
+            <h1 id='header'>Press button B</h1>
+            <button type="button" className='buttons' id='buttonA' onClick={handleButtonClick}>A</button>
+            <button type="button" className='buttons' id='buttonB' onClick={handleButtonClick}>B</button>
+            <button type="button" className='buttons' id='buttonC' onClick={handleButtonClick}>C</button>
+            <button type="button" className='buttons' id='buttonD' onClick={handleButtonClick}>D</button>
+            <h3 id='message'>{message}</h3>
         </div>
     );
 };
